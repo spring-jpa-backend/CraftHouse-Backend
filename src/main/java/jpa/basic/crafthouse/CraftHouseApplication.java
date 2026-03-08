d252ab8ae25f171da99ea923414cea0b59f941e4
재민 <silvull2t@naver.com>
2026-03-08 21:46:01 +0900

    feat: Initial commit

---
+++ src/main/java/jpa/basic/crafthouse/CraftHouseApplication.java
@@ -0,0 +1,13 @@
+package jpa.basic.crafthouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraftHouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpa/basic/crafthouse/CraftHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.basic.crafthouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftHouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
