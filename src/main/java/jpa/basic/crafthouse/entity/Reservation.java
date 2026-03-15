65c4890ccfbe4638e4b51826ceb87064f1a88523
hyun2ee2 <Hongkobi04@gmail.com>
2026-03-15 22:32:45 +0900

    feat: Entity 구현 및 ReservationStatus 추가

--- src/main/java/jpa/basic/crafthouse/entity/Reservation.java
+++ src/main/java/jpa/basic/crafthouse/entity/Reservation.java
@@ -1,4 +1,62 @@
 package jpa.basic.crafthouse.entity;
 
+import jakarta.persistence.*;
+import jpa.basic.crafthouse.enums.ReservationStatus;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "reservations")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // 예약 고유 식별자 ID
+    private Long id;
+
+    // 예약 회원 ID
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @NotFound(action = NotFoundAction.IGNORE)
+    private Member member;
+
+    // 예약 대상 클래스 ID
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "craftClass_id", nullable = false)
+    @NotFound(action = NotFoundAction.IGNORE)
+    private CraftClass craftClass;
+
+    // 예약 건 상태
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ReservationStatus status;
+
+    // 예약 생성 일시
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime reservedAt;
+
+    // 예약 취소 여부(기본값: false)
+    @Column(nullable = false)
+    private boolean cancelled = false;
+
+    // 예약 취소 일시
+    @Column(name = "cancelledAt")
+    private LocalDateTime cancelledAt;
+
+    // 예약 취소 메서드
+    public void cancel() {
+        this.cancelled = true;
+        this.cancelledAt = LocalDateTime.now();
+        this.status = ReservationStatus.CANCELLED;
+    }
 }
---
+++ src/main/java/jpa/basic/crafthouse/enums/ReservationStatus.java
@@ -0,0 +1,6 @@
+package jpa.basic.crafthouse.enums;
+
+public enum ReservationStatus {
+    CONFIRMED, // 예약 완료
+    CANCELLED  // 예약 취소
+}
